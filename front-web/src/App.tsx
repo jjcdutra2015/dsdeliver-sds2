3085992a3ba024c0029cc6d727059266bddfc132
julio.dutra <julio.dutra@sumicity.net.br>
2021-01-06 17:15:23 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar/index';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
