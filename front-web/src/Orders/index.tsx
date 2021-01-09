95cd8568e75a0e12772c47a040c3ac284705660d
julio.dutra <julio.dutra@sumicity.net.br>
2021-01-08 21:08:03 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { OrderLocationdata, Product } from './types';
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
+    const [product, setProduct] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProduct(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={product} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
